b809662ae32ef9983b33ee5754604889b7f2c0b7
Peter Arato <it.arato@gmail.com>
2023-10-12 21:05:35 -0400

    update

--- src/logo.cpp
+++ src/logo.cpp
@@ -1,12 +1,15 @@
 // #pragma once
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-const char *keywords[] = {"left", "right", "forward", "backward", "up",
-                          "down", "fn",    "if",      "else",     "for"};
+const vector<string> keywords{"left", "right", "forward", "backward", "up",
+                              "down", "fn",    "if",      "else",     "for"};
 
 enum class LexemeKind {
   Keyword,
@@ -19,25 +22,85 @@ enum class LexemeKind {
   Comma,
 };
 
-struct Lexeme {
-  union {
-    float number;
-  } data;
-  LexemeKind kind;
+union LexemeData {
+  float number;
+  string keyword;
 };
+
+struct Lexeme {
+  LexemeKind kind;
+  LexemeData data;
+
+  Lexeme(LexemeKind kind) : kind(kind), data(0.0f) {}
+  Lexeme(const Lexeme &) = default;
+  ~Lexeme() {}
+};
+
+Lexeme makeKeywordLexeme(string keyword) {
+  Lexeme out{LexemeKind::Keyword};
+  out.data.keyword = keyword;
+  return out;
+}
 
 struct Lexer {
   string code;
+  size_t ptr = 0;
 
   Lexer(string code) : code(code) {}
   Lexer(const Lexer &code) = delete;
   Lexer(Lexer &&code) = delete;
   ~Lexer() {}
 
-  vector<Lexeme> parse() const {
+  vector<Lexeme> parse() {
     vector<Lexeme> lexemes{};
+
+    while (!isEnd()) {
+      consumeSpaces();
+      char next = peek();
+      if (isalpha(next)) {
+        lexemes.push_back(readWord());
+      } else if (isdigit(next)) {
+        // lexemes.push_back(readNumber());
+      }
+    }
+
     return lexemes;
+  }
+
+  Lexeme readWord() {
+    string word = readWhile([](char c) -> bool { return isalpha(c); });
+    auto it = find(keywords.begin(), keywords.end(), word);
+    if (it == keywords.end()) {
+      return makeKeywordLexeme(*it);
+    }
+
+    exit(EXIT_FAILURE);
+  }
+
+  string readWhile(bool (*condFn)(char)) {
+    string out{};
+
+    while (!isEnd() && condFn(peek())) {
+      out.push_back(next());
+    }
+
+    return out;
+  }
+
+  bool isEnd() const { return ptr >= code.size(); }
+  char peek() const { return code.at(ptr); }
+  char next() { return code.at(ptr++); }
+
+  void consumeSpaces() {
+    readWhile([](char c) -> bool { return isspace(c); });
   }
 };
 
-int main() {}
+int main() {
+  cout << "Test parser\n";
+  Lexer lexer{"forward 10"};
+
+  cout << lexer.readWhile([](char c) -> bool { return isalpha(c); });
+
+  auto lexemes = lexer.parse();
+}
